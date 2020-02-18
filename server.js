b802ddf9ceed0510e5dbb79e697a5dc92f75af91
Sawaki <you@example.com>
2020-02-18 15:31:48 +0900

    サーバとの通信を可能にする。

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express")
+
+const mongoose = require("mongoose")
+
+const bodyParser = require("body-parser")
+
+const User = require("./models/model")
+
+const app = express()
+const port = 3001
+
+const url = "mongodb://localhost/crud"
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+mongoose.connect(url, err => {
+    if (err) {
+        throw new Error(err)
+    }
+    console.log("データベースに接続完了")
+
+    app.post("/api/users", (req, res)=>{
+        console.log(`POST来たよ`)
+        console.log(req.body)
+        res.send()
+        // new User({
+        //     name: req.body.name,
+        //     email: req.body.email
+        // }).save(err=> {
+        //     if (err) res.status(500)
+        //     res.status(200).send(`${req.body.name}のデータ作ったよ`)
+        // })
+    })
+
+
+
+    app.listen(port, err => {
+        if (err) {
+            throw new Error(err)
+        }
+        console.log(`listening on port ${port}`)
+    })
+})
+
+
+
+
+
+
+
+
---
+++ server.md
@@ -0,0 +1,45 @@
+
+シンプルなReactとデータベースのCRUDアプリを作成する。
+
+# 環境構築
+
+
+以下サーバサイド用にcrudディレクトリ内で行う。
+```
+yarn init -y
+yarn add express
+yarn add mongoose
+yarn add body-parser
+```
+
+crudディレクトリ内で
+
+```
+yarn create react-app client
+cd client
+yarn add axios
+```
+を行う。axiosはHTTPリクエスト対応のため導入。
+
+- crud
+    - client
+    - node_modules
+    - package.json
+    - server.js
+    - yarn.lock
+
+clientを適当に作る。
+clientのaxiosを作成。
+server.jsに色々書いて、接続。
+
+
+
+
+
+
+
+
+
+
+
+
