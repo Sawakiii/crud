625bd1b69c18342288e67c75489c8fb1c80441ae
Sawaki <you@example.com>
2020-02-18 16:15:36 +0900

    データのフェッチ

--- server.js
+++ server.js
@@ -20,19 +20,24 @@ mongoose.connect(url, err => {
     }
     console.log("データベースに接続完了")
 
-    app.post("/api/users", (req, res)=>{
+    app.post("/api/user", (req, res)=>{
         console.log(`POST来たよ`)
         console.log(req.body)
-        res.send()
+        // res.send() => sendを2回するとエラーが出る。
         new User({
             name: req.body.name,
             email: req.body.email
-        }).save(err=> {
-            if (err) res.status(500)
+        }).save(err => {
+            if (err) {res.status(500)}
             res.status(200).send(`${req.body.name}のデータ作ったよ`)
         })
     })
-
+    app.get("/api/user", (req, res) => {
+        User.find({}, (err, userArray)=>{
+            if (err) res.status(500).send(`失敗`)
+            res.status(200).send(userArray)
+        })
+    })
 
 
     app.listen(port, err => {
